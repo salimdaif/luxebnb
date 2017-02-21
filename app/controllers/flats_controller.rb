38e5d825cdc424e9b422dc148ab34c91c5d5b4d9
Yassine Elabbassi <yassine@elabbassi.org>
2017-02-21 11:54:58 -0300

    added city and adress to flats

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,25 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'flats/index'
+
+  get 'flats/show'
+
+  get 'flats/new'
+
+  get 'flats/create'
+
+  get 'flats/edit'
+
+  get 'flats/update'
+
+  get 'flats/destroy'
+
   devise_for :users
   resources :pages, only: [:index]
   resources :users, only: [:show]
