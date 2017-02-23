406f798abcd228626fbf1d4d48b16289edb358ec
Salim Daif <salim.daif@gmail.com>
2017-02-23 19:44:47 -0300

    HTTPS added to production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'luxebnb-salimdaif.herokuapp.com', port: 443 }
+  config.action_mailer.default_url_options = { host: 'https://luxebnb-salimdaif.herokuapp.com', port: 443 }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
