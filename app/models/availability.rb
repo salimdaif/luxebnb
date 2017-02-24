f7a5f6c225db134657b7c1136ef273e828897682
Salim Daif <salim.daif@gmail.com>
2017-02-24 11:22:46 -0300

    Fixed Booking Validations

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -2,5 +2,7 @@ class Availability < ApplicationRecord
   belongs_to :flat
 
   validates :start_date, :end_time, presence: true
+  validates :start_date, date: {after: Proc.new {Date.today}}
+  validates :end_time, date: { after: Proc.new { self.start_date}}
 
 end
