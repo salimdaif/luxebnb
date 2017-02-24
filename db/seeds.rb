020f3ea58a9adde95dc97267214359dc3fed82d9
Salim Daif <salim.daif@gmail.com>
2017-02-24 16:40:10 -0300

    seed services

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,31 +8,9 @@
 
 require 'faker'
 
-Service.create(price: 700, desc: "Gourmet Breakfast")
-Service.create(price: 900, desc: "Limo Transport")
-Service.create(price: 500, desc: "Daily Cleaning")
+Service.create(price: 250, desc: "Gourmet Breakfast")
+Service.create(price: 400, desc: "Limo Transport")
+Service.create(price: 200, desc: "Daily Cleaning")
+Service.create(price: 700, desc: "Massage Therapy")
+Service.create(price: 270, desc: "Personal Yoga")
 
-
-
-10.times do
-  flat = Flat.new(
-    title: Faker::Lorem.sentence,
-    desc: Faker::Lorem.paragraph,
-    capacity: (1..8).to_a.sample,
-    room_number: (1..5).to_a.sample,
-    bed_number: (1..10).to_a.sample,
-    bathroom_number: (1..4).to_a.sample,
-    price_night: (100..500).to_a.sample,
-    city: ["rio", "barcelona", "paris", "sao paulo"].sample,
-    address: Faker::Address.street_address,
-    user_id: 3
-  )
-  flat.save!
-  availablity = Availability.new(
-    start_date: Faker::Date.backward(23),
-    end_time: Faker::Date.forward(23),
-    available: true,
-    flat_id: flat.id
-    )
-  availablity.save!
-end
