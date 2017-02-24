9a306f431ce8d4b04f05a2e68864233629ef9bce
Yassine Elabbassi <yassine@elabbassi.org>
2017-02-24 12:11:24 -0300

    show flats on search page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -17,6 +17,12 @@ class FlatsController < ApplicationController
   def search
 
      @flats= Flat.search(params)
+
+     @hash = Gmaps4rails.build_markers(@flats) do |flat, marker|
+      marker.lat flat.latitude
+      marker.lng flat.longitude
+      #marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
+    end
    end
   def show
 
