98d7c2906cb598da05c7e8e72b961743f7d35a66
Salim Daif <salim.daif@gmail.com>
2017-02-24 16:58:33 -0300

    fix start_date

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -3,6 +3,6 @@ class Availability < ApplicationRecord
 
   validates :start_date, :end_time, presence: true
   validates :start_date, date: {after: Proc.new {Date.today}}
-  validates :end_time, date: { after: Proc.new { self.start_date}}
+  validates :end_time, date: { after: Proc.new { |obj| obj.start_date}}
 
 end
